93750ea108b3fa08ab94f7d9c228f61bff018bb8
Alan de Oliveira Gonçalves <alandeog@poli.ufrj.br>
2024-10-18 09:01:12 -0300

    Refactor package.json to update the start script for server.js

--- api/index.js
+++ api/index.js
@@ -2,23 +2,19 @@ import dotenv from "dotenv";
 import express from "express";
 import fetch from "node-fetch";
 import path from "path";
-import { fileURLToPath } from "url"; // Para manipular o caminho corretamente no ESM
+import { fileURLToPath } from "url";
 
-// Configura dotenv para carregar as variáveis de ambiente
 dotenv.config();
 
-// Para substituir o __dirname no ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const OMDB_API_KEY = process.env.OMDB_API_KEY;
 
-// Middleware para servir arquivos estáticos da pasta public
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
-// Endpoint para buscar o filme via OMDB API
 app.get("/api/search", async (req, res) => {
   const { movieName, movieYear } = req.query;
   if (!movieName) {
@@ -30,28 +26,26 @@ app.get("/api/search", async (req, res) => {
   }`;
 
   try {
-    console.log("Fetching movie data from:", url); // Log the URL
+    console.log("Fetching movie data from:", url);
     const response = await fetch(url);
     const data = await response.json();
 
     if (data.Error) {
-      console.log("Movie not found:", data.Error); // Log error from API
+      console.log("Movie not found:", data.Error);
       return res.status(404).json({ error: "Filme não encontrado." });
     }
 
     res.json(data);
   } catch (error) {
-    console.error("Error fetching movie data:", error); // Log general errors
+    console.error("Error fetching movie data:", error);
     res.status(500).json({ error: "Erro ao buscar filme." });
   }
 });
 
-// Rota padrão para a página inicial
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
-// Roda o servidor
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
--- public/js/app.js
+++ public/js/app.js
@@ -1,35 +1,27 @@
-// Seleciona os elementos necessários do DOM
 const searchButton = document.getElementById("search-button");
 const overlay = document.getElementById("modal-overlay");
 const movieName = document.getElementById("movie-name");
 const movieYear = document.getElementById("movie-year");
 const movieListContainer = document.getElementById("movie-list");
 
-// Recupera a lista de filmes armazenada no localStorage ou cria uma lista vazia caso não exista
 let movieList = JSON.parse(localStorage.getItem("movieList")) ?? [];
 
-// Handler para o evento de clique no botão de busca
 async function searchButtonClickHandler() {
   try {
-    // Constrói a URL da API com os parâmetros do nome e ano do filme
     let url = `/api/search?movieName=${movieNameParameterGenerator()}${
       movieYear.value ? `&movieYear=${movieYearParameterGenerator()}` : ""
     }`;
 
-    // Faz uma requisição à API para buscar informações sobre o filme
     const response = await fetch(url);
     const data = await response.json();
 
-    // Verifica se houve algum erro na resposta da API
     if (data.error) {
       throw new Error(data.error);
     }
 
-    // Cria e exibe o modal com as informações do filme
     createModal(data);
     overlay.classList.add("open");
   } catch (error) {
-    // Exibe um alerta de erro com SweetAlert2 em caso de falha
     Swal.fire({
       icon: "error",
       title: "Erro!",
@@ -39,7 +31,6 @@ async function searchButtonClickHandler() {
   }
 }
 
-// Gera o parâmetro do nome do filme para a URL da API
 function movieNameParameterGenerator() {
   if (movieName.value === "") {
     throw new Error("O nome do filme deve ser informado");
@@ -47,23 +38,19 @@ function movieNameParameterGenerator() {
   return movieName.value.split(" ").join("+");
 }
 
-// Gera o parâmetro do ano do filme para a URL da API
 function movieYearParameterGenerator() {
   if (movieYear.value === "") {
     return "";
   }
-  // Verifica se o ano tem exatamente 4 dígitos e se é um número válido
+
   if (movieYear.value.length !== 4 || Number.isNaN(Number(movieYear.value))) {
     throw new Error("Ano do filme inválido.");
   }
   return movieYear.value;
 }
 
-// Adiciona um filme à lista e atualiza a interface e o armazenamento local
 function addToList(data) {
-  // Verifica se o filme já está na lista
   if (isFilmAlreadOnList(data.imdbID)) {
-    // Exibe um alerta se o filme já estiver na lista
     Swal.fire({
       icon: "warning",
       title: "Aviso",
@@ -72,16 +59,14 @@ function addToList(data) {
     });
     return;
   }
-  // Adiciona o filme à lista, atualiza o localStorage e a UI
+
   movieList.push(data);
   updateLocalStorage();
   updateUI(data);
   overlay.classList.remove("open");
 }
 
-// Atualiza a interface para exibir o novo filme na lista
 function updateUI(data) {
-  // Adiciona o novo cartão do filme na interface
   movieListContainer.innerHTML += `<article id='movie-card-${data.imdbID}'>
     <img
       src="${data.Poster}"
@@ -93,7 +78,6 @@ function updateUI(data) {
   </article>`;
 }
 
-// Verifica se um filme já está na lista de filmes
 function isFilmAlreadOnList(imdbID) {
   function isThisIdFromThisMovie(movie) {
     return movie.imdbID === imdbID;
@@ -101,9 +85,7 @@ function isFilmAlreadOnList(imdbID) {
   return movieList.find(isThisIdFromThisMovie);
 }
 
-// Remove um filme da lista após a confirmação do usuário
 function removeFilmFromList(imdbID) {
-  // Exibe um modal de confirmação para o usuário usando SweetAlert2
   Swal.fire({
     title: "Tem certeza?",
     text: "Tem certeza que deseja remover o filme da lista?",
@@ -114,13 +96,11 @@ function removeFilmFromList(imdbID) {
     confirmButtonText: "Sim, remover!",
     cancelButtonText: "Não",
   }).then((result) => {
-    // Se o usuário confirmar, remove o filme da lista
     if (result.isConfirmed) {
       movieList = movieList.filter((movie) => movie.imdbID !== imdbID);
       document.getElementById(`movie-card-${imdbID}`).remove();
       updateLocalStorage();
 
-      // Exibe um modal de sucesso após a remoção
       Swal.fire({
         icon: "success",
         title: "Removido!",
@@ -131,13 +111,10 @@ function removeFilmFromList(imdbID) {
   });
 }
 
-// Atualiza o localStorage com a lista de filmes atualizada
 function updateLocalStorage() {
   localStorage.setItem("movieList", JSON.stringify(movieList));
 }
 
-// Carrega os filmes da lista ao iniciar a página
 movieList.forEach(updateUI);
 
-// Adiciona o evento de clique ao botão de busca
 searchButton.addEventListener("click", searchButtonClickHandler);
